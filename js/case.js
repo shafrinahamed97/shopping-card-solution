4018cbeb605e19ced3f4aed17badfa6d15d7fe18
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-21 13:16:24 +0600

    added price * calculate

--- js/case.js
+++ js/case.js
@@ -46,11 +46,17 @@ function updateCaseNumber(isIncrease)
         newCaseNumber = previousCaseNumber-1;
     }
     caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
 }
 
 document.getElementById('btn-case-plus').addEventListener('click', function()
 {
-    updateCaseNumber(true);
+   const newCaseNumber = updateCaseNumber(true);
+
+   const caseTotalPrice = newCaseNumber *59;
+   const caseTotalElement = document.getElementById('case-total');
+   caseTotalElement.innerText = caseTotalPrice;
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
