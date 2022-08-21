98e6d97a0ad22dc56a5ea4e2484b9f43f203ab71
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-21 13:48:55 +0600

    added calculate using function and other sytem calculated

--- js/case.js
+++ js/case.js
@@ -50,15 +50,32 @@ function updateCaseNumber(isIncrease)
     return newCaseNumber;
 }
 
+function updateCaseTotalPrice(newCaseNumber)
+{
+    
+  const caseTotalPrice = newCaseNumber * 59;
+ const caseTotalElement = document.getElementById('case-total');
+ caseTotalElement.innerText  =caseTotalPrice;
+}
+
 document.getElementById('btn-case-plus').addEventListener('click', function()
 {
    const newCaseNumber = updateCaseNumber(true);
 
-   const caseTotalPrice = newCaseNumber *59;
-   const caseTotalElement = document.getElementById('case-total');
-   caseTotalElement.innerText = caseTotalPrice;
+   updateCaseTotalPrice(newCaseNumber);
+
+//    const caseTotalPrice = newCaseNumber *59;
+//    const caseTotalElement = document.getElementById('case-total');
+//    caseTotalElement.innerText = caseTotalPrice;
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+   const newCaseNumber = updateCaseNumber(false);
+//   const caseTotalPrice = newCaseNumber * 59;
+//  const caseTotalElement = document.getElementById('case-total');
+//  caseTotalElement.innerText  =caseTotalPrice;
+
+updateCaseTotalPrice(newCaseNumber);
+
+
 })
